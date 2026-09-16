caa3c6249f13367647195df564ea39819d75b3c9
RICARDOPRF <pauloricardo96@live.com>
2026-09-16 19:43:41 -0300

    feat: add Firebase web configuration

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = Object.freeze({
+  apiKey: "AIzaSyBg_HNACiA00ZIFlvhVQWKDpU2kVJjHOVA",
+  authDomain: "lps-control-center.firebaseapp.com",
+  projectId: "lps-control-center",
+  storageBucket: "lps-control-center.firebasestorage.app",
+  messagingSenderId: "980806903223",
+  appId: "1:980806903223:web:c637d7d71ce12a107a3432",
+  measurementId: "G-HWYSC7Z46P"
+});
